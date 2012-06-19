2add60ff92ec9133fb44c89c5a464af8d2ccdc68
Artur Jaworski <artur.a.jaworski@gmail.com>
2012-06-18 21:01:24 -0400

    OuterMutation implementation

--- src/cecj/ensemble/EnsembleOuterMutationPipeline.java
+++ src/cecj/ensemble/EnsembleOuterMutationPipeline.java
@@ -3,6 +3,7 @@ package cecj.ensemble;
 import ec.BreedingPipeline;
 import ec.EvolutionState;
 import ec.Individual;
+import ec.util.MersenneTwisterFast;
 import ec.util.Parameter;
 
 public class EnsembleOuterMutationPipeline extends BreedingPipeline {
@@ -22,8 +23,30 @@ public class EnsembleOuterMutationPipeline extends BreedingPipeline {
 	@Override
 	public int produce(int min, int max, int start, int subpopulation,
 			Individual[] inds, EvolutionState state, int thread) {
-		//XXX implementacja zewnetrznej mutacji
-		return 0;
+		int n = sources[0].produce(min, max, start, subpopulation, inds, state, thread);
+		
+		if (!(sources[0] instanceof BreedingPipeline))
+            for(int q=start;q<n+start;q++)
+                inds[q] = (Individual)(inds[q].clone());
+		
+		if (!(inds[start] instanceof EnsembleIndividual))
+			state.output.fatal("OuterMutation should get EnsembleIndividuals as input");
+		
+		EnsembleSpecies species = (EnsembleSpecies) inds[start].species;
+		
+		MersenneTwisterFast rand = state.random[thread];
+		if (rand.nextDouble() < species.getOuterMutationProbability()){
+			int x = 0, y = 0;
+			do {
+				x = rand.nextInt(((EnsembleIndividual)inds[start]).getIndividualsEnsemble().length);
+				y = rand.nextInt(((EnsembleIndividual)inds[start]).getIndividualsEnsemble().length);
+			} while (x == y);
+			Individual tmp = ((EnsembleIndividual)inds[start]).getIndividualsEnsemble()[y];
+			((EnsembleIndividual)inds[start]).getIndividualsEnsemble()[y] = ((EnsembleIndividual)inds[start]).getIndividualsEnsemble()[x];
+			((EnsembleIndividual)inds[start]).getIndividualsEnsemble()[x] = tmp;
+		}
+		
+		return n;
 	}
 
 }
